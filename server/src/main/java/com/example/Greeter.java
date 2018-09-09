f5b3e88a27b045939c610215a3d6d69ee305a578
Gautam Mandsorwale <mandsorwalegautam@gmail.com>
2018-09-09 23:14:02 +0530

    Checkstyle indentation.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,22 +1,24 @@
 package com.example;
 
 /**
- * This is a class.
- */
+* This is a class.
+*/
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+/**
+* This is a constructor.
+*/
+public Greeter() {
 
-  }
+}
 
 /**
- * @param someone
- * @return greeting
- */
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+*
+* @param someone
+* @return greeting
+*/
+public final String greet(final String someone) {
+return String.format("Hello, %s!", someone);
+}
+
 }
